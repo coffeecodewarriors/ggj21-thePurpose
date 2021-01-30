d5aab86a464ab5b7c710393762f9845b558f225f
Adrian Casas <adrian.casas.fernandez@gmail.com>
2021-01-30 02:08:52 +0000

    auto center

--- js/config.js
+++ js/config.js
@@ -1,17 +1,16 @@
 export const config = {
-    width: 1280,
-    height: 720,
-    backgroundColor: 0x000000,
-    scene: [Load, Intro, Ending],
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {y: 0},
-            debug: true
-        }
-    }
-}
+  width: 1280,
+  height: 720,
+  backgroundColor: 0x000000,
+  autoCenter: true,
+  scene: [Load, Intro, Ending],
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
+      debug: true,
+    },
+  },
+};
 
-export const inventory = {
-
-}
+export const inventory = {};
